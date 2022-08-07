596a05e83cfceed6cb14b1c79bce1334f0ddd6ef
Leonardo Moura <leo@trend.io>
2022-08-07 15:04:00 -0300

    refactor: slight changes on code styling

--- src/decorators/RequestHeader.ts
+++ src/decorators/RequestHeader.ts
@@ -1,31 +1,22 @@
-import {
-  createParamDecorator,
-  ExecutionContext,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { validate, ValidationError } from 'class-validator';
 
-export const RequestHeader = createParamDecorator(
-  async (value: any, ctx: ExecutionContext) => {
-    const headers = ctx.switchToHttp().getRequest().headers;
+export const RequestHeader = createParamDecorator(async (value: any, ctx: ExecutionContext) => {
+  const headers = ctx.switchToHttp().getRequest().headers;
 
-    const dto = plainToInstance(value, headers, {
-      excludeExtraneousValues: true,
-    });
+  const dto = plainToInstance(value, headers, {
+    excludeExtraneousValues: true,
+  });
 
-    const errors: ValidationError[] = await validate(dto);
+  const errors: ValidationError[] = await validate(dto);
 
-    if (errors.length > 0) {
-      const validationErrors = errors.map((obj) =>
-        Object.values(obj.constraints),
-      );
-      throw new HttpException(
-        `Request headers validation failed: ${validationErrors}`,
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-    return dto;
-  },
-);
+  if (errors.length > 0) {
+    const validationErrors = errors.map((obj) => Object.values(obj.constraints));
+    throw new HttpException(
+      `Request headers validation failed: ${validationErrors}`,
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+  return dto;
+});
